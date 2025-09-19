d74e672fb9b704a3d8c4444f8794e82b23f8d67d
maquinanerd <30965113+maquinanerd@users.noreply.github.com>
2025-09-19 10:12:41 -0300

    66

--- executar_vocmoney_loop.bat
+++ executar_vocmoney_loop.bat
@@ -1,7 +1,14 @@
 @echo off
-:loop
 cd /d "E:\Área de Trabalho 2\VocSport"
+
+echo Ativando ambiente virtual...
 call .venv\Scripts\activate.bat
+
+echo Verificando e instalando dependencias (requirements.txt)...
+pip install -r requirements.txt
+
+:loop
+echo Iniciando o programa...
 python -m app.main
 echo ==========================================
 echo O programa terminou. Reiniciando em 5 segundos...
